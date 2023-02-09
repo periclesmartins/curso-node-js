749c2db727f56ecc59be253a8efae9cb9bd9104c
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-09 09:16:23 -0300

    Terminei aula 05c
    Criando um arquivo .txt com servidor

---
+++ aula05/aula05c.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const fs = require('fs')
+const porta = process.env.PORT 
+
+const server=http.createServer((req,res)=>{
+    fs.appendFile('teste.txt','Curso de Node.Js - CFB Cursos',(err)=>{
+        if(err)throw err
+        console.log('Arquivo criado!')
+        res.end()
+    })
+})
+
+server.listen(porta || 3000,()=>{console.log('Servidor rodando')})
